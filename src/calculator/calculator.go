34c2445b4a9bcd48307314875405f5ea488bd59a
Ján Mochňak <janmochnak@icloud.com>
2017-04-02 18:23:59 +0200

    Add basic tests

--- src/calculator/calculator.go
+++ src/calculator/calculator.go
@@ -5,79 +5,79 @@ import "math"
 
 // Calculator defines Operations of calculator
 type Calculator interface {
-    Plus(Calculator, float64)
-    Minus(Calculator, float64)
-    Multiply(Calculator, float64)
-    Divide(Calculator, float64)
-    Factorial(Calculator, float64)
-    Power(Calculator, float64)
-    NRoot(Calculator, float64)
-    Mod(Calculator, float64)
-    ClearAll(Calculator)
-    Execute(Calculator, float64)
-    Show(Calculator)
+	Plus(float64)
+	Minus(Calculator, float64)
+	Multiply(Calculator, float64)
+	Divide(Calculator, float64)
+	Factorial(Calculator, float64)
+	Power(Calculator, float64)
+	NRoot(Calculator, float64)
+	Mod(Calculator, float64)
+	ClearAll(Calculator)
+	Execute(Calculator, float64)
+	Show(Calculator)
 }
 
 // Stores current Result of equation and slot for selected Operation
 type SimpleCalc struct {
-    Result float64
-    OperationSlot func(*SimpleCalc, float64)
+	Result        float64
+	OperationSlot func(*SimpleCalc, float64)
 }
 
 // Implementation of mathematical Operations
-func(calc *SimpleCalc) Plus(operand float64) {
-    calc.Result += operand
+func (calc *SimpleCalc) Plus(operand float64) {
+	calc.Result += operand
 }
 
-func(calc *SimpleCalc) Minus(operand float64) {
-    calc.Result -= operand
+func (calc *SimpleCalc) Minus(operand float64) {
+	calc.Result -= operand
 }
 
-func(calc *SimpleCalc) Multiply(operand float64) {
-    calc.Result *= operand
+func (calc *SimpleCalc) Multiply(operand float64) {
+	calc.Result *= operand
 }
 
 // TODO Division by zero??
-func(calc *SimpleCalc) Divide(operand float64) {
-    calc.Result /= operand
+func (calc *SimpleCalc) Divide(operand float64) {
+	calc.Result /= operand
 }
 
-// Argument just for OperationSlot compatibility 
+// Argument just for OperationSlot compatibility
 // TODO int overflow
 // TODO math error if calc.Result can't be casted to Int
-func(calc *SimpleCalc) Factorial(operand float64) {
-    res := 1
-    input := int(calc.Result)
-    for k := 1; k <= input; k++ {
-        res *= k
-    }
-    calc.Result = float64(res)
+func (calc *SimpleCalc) Factorial(operand float64) {
+	res := 1
+	input := int(calc.Result)
+	for k := 1; k <= input; k++ {
+		res *= k
+	}
+	calc.Result = float64(res)
 }
 
-func(calc *SimpleCalc) Power(operand float64) {
-    calc.Result = math.Pow(calc.Result, operand)
+func (calc *SimpleCalc) Power(operand float64) {
+	calc.Result = math.Pow(calc.Result, operand)
 }
 
-func(calc *SimpleCalc) NRoot(operand float64) {
-    calc.Result = math.Pow(calc.Result, 1.0 / operand)
+func (calc *SimpleCalc) NRoot(operand float64) {
+	calc.Result = math.Pow(calc.Result, 1.0/operand)
 }
 
-func(calc *SimpleCalc) Mod(operand float64) {
-    calc.Result = math.Mod(calc.Result, operand)
+func (calc *SimpleCalc) Mod(operand float64) {
+	calc.Result = math.Mod(calc.Result, operand)
 }
 
 // Reset calculator to initial state
-func(calc *SimpleCalc) ClearAll() {
-    calc.Result = 0;
-    calc.OperationSlot = (*SimpleCalc).Plus
+func (calc *SimpleCalc) ClearAll() {
+	calc.Result = 0
+	calc.OperationSlot = (*SimpleCalc).Plus
 }
 
 // Executes selected Operation
-func(calc *SimpleCalc) Execute(operand float64) {
-    calc.OperationSlot(calc, operand)
+func (calc *SimpleCalc) Execute(operand float64) {
+	calc.OperationSlot(calc, operand)
 }
 
 // Shows Result
-func(calc *SimpleCalc) Show() {
-    fmt.Println(calc.Result)
+func (calc *SimpleCalc) Show() {
+	fmt.Println(calc.Result)
 }
--- src/main.go
+++ src/main.go
@@ -1,26 +1,25 @@
 package main
 
-import "calculator"
+import "./calculator"
 
 func main() {
-    calc := calculator.SimpleCalc{Result:0.0, OperationSlot:(*calculator.SimpleCalc).Plus}
-    calc.Execute(8.0)
-    calc.OperationSlot = (*calculator.SimpleCalc).Minus
-    calc.Execute(3.0)
-    calc.Show()
-    calc.OperationSlot = (*calculator.SimpleCalc).Factorial
-    calc.Execute(0.0)
-    calc.Show()
-    calc.OperationSlot = (*calculator.SimpleCalc).Power
-    calc.Execute(2)
-    calc.Show()
-    calc.ClearAll();
-    calc.Execute(29)
-    calc.OperationSlot = (*calculator.SimpleCalc).Mod
-    calc.Execute(5)
-    calc.Show()
-    calc.OperationSlot = (*calculator.SimpleCalc).NRoot
-    calc.Execute(2)
-    calc.Show()
+	calc := calculator.SimpleCalc{Result: 0.0, OperationSlot: (*calculator.SimpleCalc).Plus}
+	calc.Execute(8.0)
+	calc.OperationSlot = (*calculator.SimpleCalc).Minus
+	calc.Execute(3.0)
+	calc.Show()
+	calc.OperationSlot = (*calculator.SimpleCalc).Factorial
+	calc.Execute(0.0)
+	calc.Show()
+	calc.OperationSlot = (*calculator.SimpleCalc).Power
+	calc.Execute(2)
+	calc.Show()
+	calc.ClearAll()
+	calc.Execute(29)
+	calc.OperationSlot = (*calculator.SimpleCalc).Mod
+	calc.Execute(5)
+	calc.Show()
+	calc.OperationSlot = (*calculator.SimpleCalc).NRoot
+	calc.Execute(2)
+	calc.Show()
 }
-
