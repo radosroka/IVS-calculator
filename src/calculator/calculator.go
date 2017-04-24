cfcd1e4cda5831eca87ca03a1a8aadb6f24d7b0a
Radovan Sroka <rsroka@redhat.com>
2017-04-24 13:48:27 +0200

    Added license

--- src/calculator/calculator.go
+++ src/calculator/calculator.go
@@ -1,3 +1,24 @@
+/* IVS-calculator
+ * Copyright (C) 2017	Radovan Sroka <xsroka00@stud.fit.vutbr.cz>
+ * 						Tomáš Sýkora <xsykor25@stud.fit.vutbr.cz>
+ *						Michal Cyprian <xcypri01@stud.fit.vutbr.cz>
+ *						Jan Mochnak <xmochn00@stud.fit.vutbr.cz>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
 package calculator
 
 import (
